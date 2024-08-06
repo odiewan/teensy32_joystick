1d0626cbe67d2253a2356d4d42966a1c508b92b9
Odie <odie@odiesystems.com>
2024-08-05 17:49:54 -0700

    refactor io points to match  new teensy breakout board

--- teensy32_joystick/teensy32_joystick.ino
+++ teensy32_joystick/teensy32_joystick.ino
@@ -1,49 +1,48 @@
-/* Basic USB Joystick Example
-   Teensy becomes a USB joystick
-
-   You must select Joystick from the "Tools > USB Type" menu
-
-   Pushbuttons should be connected to digital pins 0 and 1.
-   Wire each button between the digital pin and ground.
-   Potentiometers should be connected to analog inputs 0 to 1.
-
-   This example code is in the public domain.
-*/
+//---set usb type as flight sim controls + joystick
 #include <Arduino.h>
 #include <serialPrint.h>
 
-#define AXIS_0_PIN  19
-#define AXIS_1_PIN  20
-#define AXIS_2_PIN  21
-#define AXIS_3_PIN  22
-
-#define LED_MODULO  25
-
-#define BTN_PIN_10  11
-#define BTN_PIN_09  10
-#define BTN_PIN_08  9
-#define BTN_PIN_07  8
-#define BTN_PIN_06  7
-#define BTN_PIN_05  6
-#define BTN_PIN_04  5
-#define BTN_PIN_03  4
-#define BTN_PIN_02  3
-#define BTN_PIN_01  2
+#define AXIS_0_PIN  14
+#define AXIS_1_PIN  15
+#define AXIS_2_PIN  16
+#define AXIS_3_PIN  17
+#define BTN0_PIN    2
+#define BTN1_PIN    3
+#define BTN2_PIN    4
+#define LED0_PIN    8
+#define LED1_PIN    9
+// #define BTN_PIN_09  10
+// #define BTN_PIN_08  9
+// #define BTN_PIN_07  8
+// #define BTN_PIN_06  7
+// #define BTN_PIN_05  6
+// #define BTN_PIN_04  5
+// #define BTN_PIN_03  4
+// #define BTN_PIN_02  3
+// #define BTN_PIN_01  2
 
 #define AIN_MAX 1023
 #define AIN_MID 512
 
-bool serialOk;
+#define LED_MODULO  25
+
+
 int tmr;
+
+bool serialOk;
 uint32_t iCount;
 
 int ain00;
 int ain01;
 int ain02;
 int ain03;
+int ain04;
+int ain05;
+// int ain06;
 
 bool jAxisX_rev;
 bool jAxisY_rev;
+bool jAxisZ_rev;
 
 bool btn0;
 bool btn1;
@@ -51,6 +50,9 @@ bool btn2;
 bool btn3;
 bool btn4;
 bool btn5;
+
+bool led0;
+bool led1;
 
 bool jBtn1;
 bool jBtn2;
@@ -61,6 +63,7 @@ bool jBtn6;
 
 int jAxisY;
 int jAxisX;
+int jAxisZ;
 
 //=============================================================================
 void ledToggle() {
@@ -74,9 +77,11 @@ void setup() {
   iCount = 0;
   tmr = 25;
   jAxisX_rev = true;
-  jAxisY_rev = true;
+  jAxisY_rev = false;
+  jAxisZ_rev = true;
   jAxisX = 0;
-  jAxisX = 0;
+  jAxisY = 0;
+  jAxisZ = 0;
 
 
   btn0 = 0;
@@ -85,6 +90,9 @@ void setup() {
   btn3 = 0;
   btn4 = 0;
   btn5 = 0;
+  led0 = 0;
+  led1 = 0;
+
   jBtn1 = 0;
   jBtn2 = 0;
   jBtn3 = 0;
@@ -92,25 +100,20 @@ void setup() {
   jBtn5 = 0;
   jBtn6 = 0;
 
-
   pinMode(AXIS_0_PIN, INPUT);
   pinMode(AXIS_1_PIN, INPUT);
   pinMode(AXIS_2_PIN, INPUT);
   pinMode(AXIS_3_PIN, INPUT);
 
-  pinMode(BTN_PIN_10, INPUT);
-  pinMode(BTN_PIN_09, INPUT);
-
-  pinMode(BTN_PIN_08, INPUT_PULLUP);
-  pinMode(BTN_PIN_07, INPUT_PULLUP);
-  pinMode(BTN_PIN_06, INPUT_PULLUP);
-  pinMode(BTN_PIN_05, INPUT_PULLUP);
-  pinMode(BTN_PIN_04, INPUT_PULLUP);
-  pinMode(BTN_PIN_03, INPUT_PULLUP);
-  pinMode(BTN_PIN_02, INPUT_PULLUP);
-  pinMode(BTN_PIN_01, INPUT_PULLUP);
+  pinMode(BTN0_PIN, INPUT_PULLUP);
+  pinMode(BTN1_PIN, INPUT_PULLUP);
+  pinMode(BTN2_PIN, INPUT_PULLUP);
 
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED0_PIN, OUTPUT);
+  pinMode(LED1_PIN, OUTPUT);
+
+
 
   Serial.begin(9600);
 
@@ -122,7 +125,7 @@ void setup() {
     delay(250);
 
     tmr--;
-    }
+  }
 
   Serial.println("Serial Port OK");
 
@@ -137,7 +140,7 @@ void taskSerial() {
     ledToggle();
 
   // if (iCount % 25 == 0) {
-    tmpStr = " ";
+    tmpStr = " teensy3.2 joystick";
 
     tmpStr += " iC:";
     tmpStr += String(iCount);
@@ -154,23 +157,44 @@ void taskSerial() {
     tmpStr += " a3:";
     tmpStr += ain03;
 
-    tmpStr += "| jAxisX:p";
-    tmpStr += AXIS_0_PIN;
-    tmpStr += " :";
-    tmpStr += jAxisX;
-    tmpStr += " r:";
-    tmpStr += jAxisX_rev;
+    // tmpStr += " a4:";
+    // tmpStr += ain04;
 
-    tmpStr += " jAxisY:p";
-    tmpStr += AXIS_1_PIN;
-    tmpStr += " :";
-    tmpStr += jAxisY;
-    tmpStr += " r:";
-    tmpStr += jAxisY_rev;
+    // tmpStr += " a5:";
+    // tmpStr += ain05;
+
+    // tmpStr += " a6:";
+    // tmpStr += ain06;
+
+
+    // tmpStr += " a3:";
+    // tmpStr += ain03;
+
+    // tmpStr += "| jX:p";
+    // tmpStr += AXIS_0_PIN;
+    // tmpStr += " :";
+    // tmpStr += jAxisX;
+    // tmpStr += " r:";
+    // tmpStr += jAxisX_rev;
+
+    // tmpStr += " jY:p";
+    // tmpStr += AXIS_1_PIN;
+    // tmpStr += " :";
+    // tmpStr += jAxisY;
+    // tmpStr += " r:";
+    // tmpStr += jAxisY_rev;
+
+    // tmpStr += " jZ:p";
+    // tmpStr += AXIS_2_PIN;
+    // tmpStr += " :";
+    // tmpStr += jAxisZ;
+    // tmpStr += " r:";
+    // tmpStr += jAxisY_rev;
     serPrnt(tmpStr);
 
     serPrntV("jBtn1", jBtn1);
     serPrntV("jBtn2", jBtn2);
+    serPrntV("jBtn3", jBtn3);
 
     serPrntNL(">");
 
@@ -184,23 +208,28 @@ void taskSerial() {
 void loop() {
   iCount++;
 
-  jBtn1 = digitalRead(BTN_PIN_01);
-  jBtn2 = digitalRead(BTN_PIN_02);
-  jBtn3 = digitalRead(BTN_PIN_03);
-  jBtn4 = digitalRead(BTN_PIN_04);
-  jBtn5 = digitalRead(BTN_PIN_05);
-  jBtn6 = digitalRead(BTN_PIN_06);
+  btn0 = !digitalRead(BTN0_PIN);
+  btn1 = !digitalRead(BTN1_PIN);
+  btn2 = !digitalRead(BTN2_PIN);
+  // jBtn3 = digitalRead(BTN_PIN_03);
+  // jBtn4 = digitalRead(BTN_PIN_04);
+  // jBtn5 = digitalRead(BTN_PIN_05);
+  // jBtn6 = digitalRead(BTN_PIN_06);
 
 
 
-  ain00 = analogRead(AXIS_1_PIN);
-  ain01 = analogRead(AXIS_0_PIN);
+  ain00 = analogRead(AXIS_0_PIN);
+  ain01 = analogRead(AXIS_1_PIN);
   ain02 = analogRead(AXIS_2_PIN);
   ain03 = analogRead(AXIS_3_PIN);
 
+  led0 = btn1;
+  led1 = btn2;
+
+
   jAxisX = map(ain00, 0, 1023, 0, 1023);
   jAxisY = map(ain01, 0, 1023, 0, 1023);
-
+  jAxisZ = map(ain02, 0, 1023, 0, 1023);
 
 
   if(jAxisX_rev) {
@@ -212,27 +241,39 @@ void loop() {
     jAxisY = AIN_MAX - jAxisY;
   }
 
+  if(jAxisZ_rev) {
+    jAxisZ = AIN_MAX - jAxisZ;
+  }
 
+  jBtn1 = btn0;
+  jBtn2 = btn1;
+  jBtn3 = btn2;
+
+
+
+  digitalWrite(LED0_PIN, led0);
+  digitalWrite(LED1_PIN, led1);
 
   // read analog inputs and set X-Y position
   Joystick.X(jAxisX);
   Joystick.Y(jAxisY);
-  Joystick.Z(AIN_MID);
+  Joystick.Z(jAxisZ);
   Joystick.Zrotate(AIN_MID);
 
   Joystick.slider(AIN_MID);
   Joystick.sliderLeft(AIN_MID);
   Joystick.sliderRight(AIN_MID);
 
-  Joystick.button(1, !jBtn1);
-  Joystick.button(2, !jBtn2);
-  Joystick.button(3, !jBtn3);
-  Joystick.button(4, !jBtn4);
-  Joystick.button(5, !jBtn5);
-  Joystick.button(6, !jBtn6);
+  Joystick.button(1, jBtn1);
+  Joystick.button(2, jBtn2);
+  Joystick.button(3, jBtn3);
+  // Joystick.button(3, jBtn3);
+  // Joystick.button(4, jBtn4);
+  // Joystick.button(5, jBtn5);
+  // Joystick.button(6, jBtn6);
 
   taskSerial();
 
   // a brief delay, so this runs 20 times per second
-  delay(10);
+  delay(20);
 }
